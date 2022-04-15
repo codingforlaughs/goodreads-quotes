22e32a4e86b28d28bc345a3734a1035ef651e5d7
codingforlaughs <89112799+codingforlaughs@users.noreply.github.com>
2022-04-14 21:47:08 -0400

    Update __init__.py
    update init to the latest website structure and added the ability to pull quotes from multiple pages. Also changed daily quote to recent quote.

--- goodreads_quotes/__init__.py
+++ goodreads_quotes/__init__.py
@@ -17,7 +17,7 @@ __version__ = '0.1.0'
 class GoodreadsException(Exception):
     """Goodreads exception
     """
-
+    
     def __init__(self, msg):
         self.msg = msg
 
@@ -41,30 +41,11 @@ class GoodreadsException(Exception):
 class Goodreads:
 
     @staticmethod
-    def get_daily_quote():
+    def get_recent_quotes():
+        from random import randint
+        i = randint(1,100)
         try:
-            quotes_page = requests.get('https://www.goodreads.com/quotes')
-        except Timeout as e:
-            raise GoodreadsException(e)
-        except RequestException as e:
-            raise GoodreadsException(e)
-
-        tree = html.fromstring(quotes_page.content)
-
-        quote_text = tree.xpath('//div[@id="quoteoftheday"]/div[1]/i/text()')[0]
-        author = tree.xpath('//div[@id="quoteoftheday"]/div[2]/strong/div/a/text()')[0]
-
-        quote = {
-            'quote': quote_text,
-            'author': author
-        }
-
-        return quote
-
-    @staticmethod
-    def get_popular_quotes():
-        try:
-            quotes_page = requests.get('https://www.goodreads.com/quotes')
+            quotes_page = requests.get(f'https://www.goodreads.com/quotes/recently_added?page={i}')
         except Timeout as e:
             raise GoodreadsException(e)
         except RequestException as e:
@@ -75,8 +56,41 @@ class Goodreads:
         quotes = []
         for quote_div in tree.xpath('//div[@class="quoteText"]'):
             quote_text = quote_div.xpath('text()')[0].strip().replace('“', '').replace('”', '')
-            author = quote_div.xpath('a/text()')[0].strip()
+            # author = quote_div.xpath('a/text()')[0].strip()
+        for quote_div in tree.xpath('//span[@class="authorOrTitle"]'):
+            author = quote_div.xpath('text()')[0].strip().replace('“', '').replace('”', '')
+            # author = quote_div.xpath('a/text()')[0].strip()
+              
+            quote = {
+                'quote': quote_text,
+                'author': author
+            }
 
+            quotes.append(quote)
+
+        return quotes
+
+    @staticmethod
+    def get_popular_quotes():
+        from random import randint
+        i = randint(1,100)
+        try:
+            quotes_page = requests.get(f'https://www.goodreads.com/quotes?page={i}')
+        except Timeout as e:
+            raise GoodreadsException(e)
+        except RequestException as e:
+            raise GoodreadsException(e)
+
+        tree = html.fromstring(quotes_page.content)
+
+        quotes = []
+        for quote_div in tree.xpath('//div[@class="quoteText"]'):
+            quote_text = quote_div.xpath('text()')[0].strip().replace('“', '').replace('”', '')
+            # author = quote_div.xpath('a/text()')[0].strip()
+        for quote_div in tree.xpath('//span[@class="authorOrTitle"]'):
+            author = quote_div.xpath('text()')[0].strip().replace('“', '').replace('”', '')
+            # author = quote_div.xpath('a/text()')[0].strip()
+              
             quote = {
                 'quote': quote_text,
                 'author': author
@@ -88,7 +102,7 @@ class Goodreads:
 
     @staticmethod
     def get_daily_quote_as_json():
-        return json.dumps(Goodreads.get_daily_quote())
+        return json.dumps(Goodreads.get_recent_quote())
 
     @staticmethod
     def get_popular_quotes_as_json():
